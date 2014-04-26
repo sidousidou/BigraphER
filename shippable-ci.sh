4d53474a2ee722968d62fa419128e492e13f05b8
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2014-04-27 00:30:18 +0100

    Fix opam init by suppressing user input.

--- shippable-ci.sh
+++ shippable-ci.sh
@@ -7,7 +7,7 @@ export DCS_REPO='http://www.dcs.gla.ac.uk/~michele/dcs-opam-repository/'
 export OPAM_PACKAGES='ocp-build minisat'
 
 # Install packages from OPAM
-opam init
+opam init -q -a --sh
 eval `opam config -env`
 opam repository -q -y add dcs ${DCS_REPO}
 opam install -q -y ${OPAM_PACKAGES}
