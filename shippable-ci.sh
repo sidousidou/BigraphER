38149aa6811f9f5b36e5c4ace794589d4925a046
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2014-04-27 00:14:35 +0100

    Add opam init to shippable script.

--- shippable-ci.sh
+++ shippable-ci.sh
@@ -1,36 +1,14 @@
 #!/bin/sh
 
-# OCaml version to install
-#export OCAML_VERSION='4.01.0'
-
-# OPAM version to install
-#export OPAM_VERSION='1.1.1'
-
 # OPAM DCS repository
 export DCS_REPO='http://www.dcs.gla.ac.uk/~michele/dcs-opam-repository/'
 
 # OPAM packages needed to build tests
 export OPAM_PACKAGES='ocp-build minisat'
 
-# Install OCaml
-#sudo apt-get update -qq
-#sudo apt-get install -qq ocaml
-
-echo 'OCaml version'
-ocaml -version
-
-# Install OPAM
-#curl -L https://github.com/ocaml/opam/archive/${OPAM_VERSION}.tar.gz | tar xz -C /tmp
-#pushd /tmp/opam-${OPAM_VERSION}
-#./configure
-#make
-#make install
-#opam init
-#eval `opam config -env`
-#popd
-
 # Install packages from OPAM
-#opam switch ${OCAML_VERSION}  
+opam init
+eval `opam config -env`
 opam repository -q -y add dcs ${DCS_REPO}
 opam install -q -y ${OPAM_PACKAGES}
 
