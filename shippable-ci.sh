8c73e42647bfd5563fbceecac21bff0751b27e24
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2014-04-27 00:37:20 +0100

    Add minisat in the shippable configuration.

--- shippable-ci.sh
+++ shippable-ci.sh
@@ -6,6 +6,11 @@ export DCS_REPO='http://www.dcs.gla.ac.uk/~michele/dcs-opam-repository/'
 # OPAM packages needed to build tests
 export OPAM_PACKAGES='ocp-build minisat'
 
+# Install MiniSat
+#curl -L https://github.com/ocaml/opam/archive/${OPAM_VERSION}.tar.gz | tar xz -C /tmp
+#-#pushd /tmp/opam-${OPAM_VERSION}
+#-#./configure
+
 # Install packages from OPAM
 opam init -q -a --sh
 eval `opam config -env`
