38bb5cf266deb1b84a80410fb813c6bc9b4a8c7b
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2014-07-09 16:18:31 +0100

    Fix paths in the script

--- scripts/shippable-link.sh
+++ scripts/shippable-link.sh
@@ -6,7 +6,7 @@ echo INSTALLING BIGRAPHER
 ocp-build install bigraph
 
 echo BYTE-CODE COMPILATION TEST
-ocamlfind ocamlc -o ../shippable/ex.byte -package bigraph -linkpkg ../examples/ex.ml
+ocamlfind ocamlc -o ./shippable/ex.byte -package bigraph -linkpkg ./examples/ex.ml
 
 echo NATIVE-CODE COMPILATION TEST
-ocamlfind opt -o ../shippable/ex.asm -package bigraph -linkpkg ../examples/ex.ml
+ocamlfind opt -o ./shippable/ex.asm -package bigraph -linkpkg ./examples/ex.ml
