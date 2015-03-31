4653026193ddceacb625ceaed8c6b417fd4dddad
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2015-03-31 09:13:05 +0100

    Fix typo in eval command.

--- scripts/shippable-opam.sh
+++ scripts/shippable-opam.sh
@@ -8,6 +8,6 @@ export OPAM_PACKAGES='ocp-build minisat'
 
 # Install packages from OPAM
 opam init -q -a --sh
-eval `opam config -env`
+eval `opam config env`
 opam repository -q -y add dcs ${DCS_REPO}
 opam install -q -y ${OPAM_PACKAGES}
