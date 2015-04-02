e384a96c1d98e414ae0b5d3e1b72c8936fbb07a2
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2015-04-02 04:49:21 +0100

    Check output in the tests.

--- scripts/shippable-build.sh
+++ scripts/shippable-build.sh
@@ -2,9 +2,13 @@
 
 eval `opam config env`
 
+PREFIX=$(opam config var prefix)
+
 echo CLEANING ...
+ocaml setup.ml -uninstall
+ocamlfind remove bigraph
 ocaml setup.ml -distclean
 
 echo COMPILING ...
-ocaml setup.ml -configure --enable-tests
+ocaml setup.ml -configure --enable-tests --prefix $PREFIX
 ocaml setup.ml -build
--- scripts/shippable-link.sh
+++ scripts/shippable-link.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+ROOT=./shippable
+
 eval `opam config env`
 
-echo INSTALLING BIGRAPHER
-ocp-build install bigraph
+echo INSTALLING BIGRAPH LIBRARY
+ocaml setup.ml -install
+
+echo CLEANING ...
+ocaml setup.ml -distclean
+
+cp ./examples/ex.ml $ROOT/tmp/ex.ml
 
 echo BYTE-CODE COMPILATION TEST
-ocamlfind ocamlc -o ./shippable/ex.byte -package bigraph -linkpkg ./examples/ex.ml
+ocamlfind ocamlc -o $ROOT/ex.byte -package bigraph -linkpkg $ROOT/tmp/ex.ml
+$ROOT/ex.byte | cmp -s -  ./tests/files/ex.reference
 
 echo NATIVE-CODE COMPILATION TEST
-ocamlfind opt -o ./shippable/ex.asm -package bigraph -linkpkg ./examples/ex.ml
+ocamlfind ocamlopt -o $ROOT/ex.asm -package bigraph -linkpkg $ROOT/tmp/ex.ml
+$ROOT/ex.native | cmp -s -  ./tests/files/ex.reference
