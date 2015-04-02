aaa1d23491a2c379acbc96619b0f296459a13623
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2015-04-02 14:04:37 +0100

    Test jUnit output.

--- tests/test_brs.ml
+++ tests/test_brs.ml
@@ -58,51 +58,39 @@ let sreacts =
       [ { Sbrs.rdx = r; rct = r_p; rate = 2.0; };
         { Sbrs.rdx = g; rct = r; rate = 4.0; } ] ]
 
+let assert_eq_int name reference out =
+  if out = reference then ""
+  else  sprintf "<failure>%s: %-8d != %-8d</failure>" name out reference
+    
 let () =
   Random.self_init ();
-  let iter_f _ _ = () in
+  let iter_f _ _ = ()
+  and states_reference = 30
+  and reacts_reference = 29
+  and occurs_reference = 4495
+  and print_res s r o =
+    printf "States              : %s\n\
+            Reactions           : %s\n\
+            Occurrences         : %s\n"
+           (colorise `bold (colorise `blue (sprintf "%-8d" s)))
+           (colorise `bold (colorise `blue (sprintf "%-8d" r)))
+           (colorise `bold (colorise `blue (sprintf "%-8d" o))) in 
   match Sys.argv.(1) with
-  | "brs" -> (
-      let (_, stats) = 
-        Brs.bfs s reacts 1000 50 iter_f in
-      printf "States              : %s\n\
-              Reactions           : %s\n\
-              Occurrences         : %s\n"
-        (colorise `bold (colorise `blue (sprintf "%-8d" stats.Brs.s)))
-        (colorise `bold (colorise `blue (sprintf "%-8d" stats.Brs.r)))
-        (colorise `bold (colorise `blue (sprintf "%-8d" stats.Brs.o)));
-    )
-  | "sym_brs" -> (
-      let (_, stats) = 
-	Brs.sim s reacts 1000 50 iter_f in
-      printf "States              : %s\n\
-              Reactions           : %s\n\
-              Occurrences         : %s\n"
-        (colorise `bold (colorise `blue (sprintf "%-8d" stats.Brs.s)))
-        (colorise `bold (colorise `blue (sprintf "%-8d" stats.Brs.r)))
-        (colorise `bold (colorise `blue (sprintf "%-8d" stats.Brs.o)));
-    )
-  | "sbrs" -> (
-      let (_, stats) = 
-	Sbrs.bfs s sreacts 1000 50 iter_f in
-      printf "States              : %s\n\
-              Reactions           : %s\n\
-              Occurrences         : %s\n"
-	(colorise `bold (colorise `blue (sprintf "%-8d" stats.Sbrs.s)))
-        (colorise `bold (colorise `blue (sprintf "%-8d" stats.Sbrs.r)))
-        (colorise `bold (colorise `blue (sprintf "%-8d" stats.Sbrs.o)));
-    )
-  | "sym_sbrs" -> (
-      let (_, stats) = 
-	Sbrs.sim s sreacts 5000.0 50 iter_f in 
-      printf "States              : %s\n\
-              Reactions           : %s\n\
-              Occurrences         : %s\n"
-	(colorise `bold (colorise `blue (sprintf "%-8d" stats.Sbrs.s)))
-	(colorise `bold (colorise `blue (sprintf "%-8d" stats.Sbrs.r)))
-	(colorise `bold (colorise `blue (sprintf "%-8d" stats.Sbrs.o)));
-    )
+  | "brs" ->
+     let (_, stats) = 
+       Brs.bfs s reacts 1000 50 iter_f in
+     print_res stats.Brs.s stats.Brs.r stats.Brs.o;
+  | "sim_brs" -> 
+     let (_, stats) = 
+       Brs.sim s reacts 1000 50 iter_f in
+     print_res stats.Brs.s stats.Brs.r stats.Brs.o;
+  | "sbrs" ->
+     let (_, stats) = 
+       Sbrs.bfs s sreacts 1000 50 iter_f in
+     print_res stats.Sbrs.s stats.Sbrs.r stats.Sbrs.o;
+  | "sim_sbrs" ->
+     let (_, stats) = 
+       Sbrs.sim s sreacts 5000.0 50 iter_f in 
+     print_res stats.Sbrs.s stats.Sbrs.r stats.Sbrs.o;
   | _ -> exit 1
-
-  
-
+	      
