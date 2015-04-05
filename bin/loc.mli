78f2cd4d817e6ef7f5ddb70df3de690eb4f2d597
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2015-04-05 21:02:48 +0100

    Add print_loc to the public interface.

--- bin/loc.mli
+++ bin/loc.mli
@@ -1,10 +1,12 @@
 type t = {
   lstart: Lexing.position;
-  lend: Lexing.position;
+  lend:   Lexing.position;
 }
 
 val print_loc : Format.formatter -> t -> unit
 
+val print_pos : Format.formatter -> t -> unit
+  
 val curr : Lexing.lexbuf -> t
 
 val loc : Lexing.position -> Lexing.position -> t
