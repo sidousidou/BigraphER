73e178c6f3a74fb42f22f18d2ddcf032c738c9b5
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2015-04-05 21:00:51 +0100

    Add function for warnings.

--- bin/loc.ml
+++ bin/loc.ml
@@ -2,8 +2,8 @@ open Lexing
 open Format
 
 type t = {
-  lstart: Lexing.position;
-  lend: Lexing.position;
+    lstart: Lexing.position;
+    lend:   Lexing.position;
 }
 
 let print_loc fmt loc =
@@ -12,16 +12,23 @@ let print_loc fmt loc =
   let off0 = p0.pos_cnum - p0.pos_bol
   and off1 = p1.pos_cnum - p0.pos_bol in (* same line *)
   fprintf fmt "File \"%s\", line %i, characters %i-%i"
-    p0.pos_fname p0.pos_lnum off0 off1
+	  p0.pos_fname p0.pos_lnum off0 off1
 
+let print_pos fmt loc =
+  let p0 = loc.lstart 
+  and p1 = loc.lend in
+  let off0 = p0.pos_cnum - p0.pos_bol
+  and off1 = p1.pos_cnum - p0.pos_bol in (* same line *)
+  fprintf fmt "line %i, characters %i-%i" p0.pos_lnum off0 off1
+	  
 let curr lexbuf = {
-  lstart = lexbuf.lex_start_p;
-  lend = lexbuf.lex_curr_p;
-}
+    lstart = lexbuf.lex_start_p;
+    lend = lexbuf.lex_curr_p;
+  }
 
 let loc lstart lend = { lstart; lend }
-
+			
 let dummy_loc = {
-  lstart = Lexing.dummy_pos;
-  lend = Lexing.dummy_pos;
+    lstart = Lexing.dummy_pos;
+    lend = Lexing.dummy_pos;
 }
