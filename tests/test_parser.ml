35104e05fef94400c9c370ae28894bb51265b7db
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2015-04-11 04:19:51 +0100

    Fix comparison with reference output.

--- tests/test_parser.ml
+++ tests/test_parser.ml
@@ -2,14 +2,14 @@ open Printf
 open Junit
 
 (* parse a list of files-names *)
-let rec read_lines file out =
-  try read_lines file ((input_line file) :: out)
+let rec read_lines out file =
+  try read_lines ((input_line file) :: out) file
   with 
   | End_of_file -> (close_in file; List.rev out)
  
 let parse path =
   let file = open_in path in
-  read_lines file []
+  read_lines [] file 
 
 let bin = "./bigrapher.native"
 let flag = "--debug"
@@ -19,23 +19,33 @@ let ext = ".reference"
 let attr_string = [("type", "ASSERT_OUTPUT");
 		   ("message", "Output is not the same")]
 
+let rec compare (out : string list) (reference : string list) =
+  match (out, reference) with
+  | ([], []) -> []
+  | (l :: out, l_ref :: reference) ->
+     (if String.compare l l_ref = 0 then compare out reference
+      else [xml_block "failure" attr_string [sprintf "%s != %s" l l_ref]])
+  | ([], l_ref :: _) ->
+     [xml_block "failure" attr_string [sprintf "nil != %s" l_ref]]
+  | (l :: _ , []) ->
+     [xml_block "failure" attr_string [sprintf "%s != nil" l]]
+
 (* Args: PATH OUT-PATH FNAME *)  
 let () =
-  let tests_files = parse Sys.argv.(1) in
-  let testcases =
-    List.map (fun f ->
-	      let name = Filename.chop_extension (Filename.basename f) in
-	      let chan_in = Unix.open_process_in (bin ^ " " ^ f ^ " " ^ flag) in
-	      let std_out = read_lines chan_in [] in	      
-	      (name,
-	       "test_parser.ml",
-	       xml_block "system-out" [] [String.concat "\n" std_out],
-	       let ref_out = Filename.concat path (name ^ ext) in
-	       try List.map (fun (l, l_ref) ->
-			     if String.compare l l_ref = 0 then ""
-			     else xml_block "failure" attr_string [sprintf "%s != %s" l l_ref])
-			    (List.combine std_out (read_lines (open_in ref_out) []))
-	       with
-	       | e ->
-		  [xml_block "error" attr_err [Printexc.to_string e]])) tests_files in
+  let (testcases : (string * string * string * string list) list) =
+    parse Sys.argv.(1)
+    |> List.map (fun f ->
+		 let name = Filename.chop_extension (Filename.basename f) in
+		 let chan_in = Unix.open_process_in (bin ^ " " ^ f ^ " " ^ flag) in
+		 let std_out = read_lines [] chan_in in	      
+		 (name,
+		  "test_parser.ml",
+		  xml_block "system-out" [] [String.concat "\n" std_out],
+		  let ref_out =
+		    Filename.concat path (name ^ ext)
+		    |> open_in
+		    |> read_lines [] in
+		  try compare std_out ref_out with
+		  | e ->
+		     [xml_block "error" attr_err [Printexc.to_string e]])) in
   write_xml (testsuite "test_parser" testcases) Sys.argv.(2) Sys.argv.(3)
