fe548372a82437094229fc64f5d4e570cd3fa3a2
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2015-04-12 12:01:42 +0100

    Fix documentation on raised exceptions.

--- lib/export.mli
+++ lib/export.mli
@@ -19,16 +19,19 @@
 (** [write_big b name path v] writes an svg representation of bigraph
     [b] on file [name] in directory [path]. Argument [v] is a flag for
     verbosity.
-    @raise Failure when [dot] terminates unexpectedly.*)
+    @raise Unix.Unix_error when an underlying system call signals an error. *)
 val write_big : Big.bg -> string -> string -> bool -> unit 
 
-(** Same as {!Export.write_big} but for transition systems. *)
+(** Same as {!Export.write_big} but for transition systems. 
+    @raise Unix.Unix_error when an underlying system call signals an error. *)
 val write_ts: Brs.ts -> string -> string -> bool -> unit
 
-(** Same as {!Export.write_big} but for Continuous Time Markov Chains. *)
+(** Same as {!Export.write_big} but for Continuous Time Markov Chains. 
+    @raise Unix.Unix_error when an underlying system call signals an error. *)
 val write_ctmc: Sbrs.ctmc -> string -> string -> bool -> unit
 
-(** Export a transition system to a text file in PRISM format. *)
+(** Export a transition system to a text file in PRISM format. 
+    @raise Unix.Unix_error when an underlying system call signals an error. *)
 val write_ts_prism: Brs.ts -> string -> string -> bool -> unit
 
 (** Same as {!Export.write_ts_prism} but for Continuous Time Markov Chains. *)
