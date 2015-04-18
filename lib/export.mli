38391411b1f6b82fcb223481f221603d2aeee5a1
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2015-04-18 21:12:02 +0100

    Reword documentation in module Export

--- lib/export.mli
+++ lib/export.mli
@@ -9,14 +9,14 @@ type error =
   | Internal_error of Unix.error * string * string
   | Sys of string
 
-(** Raised by all the functions of this module when an error is encountered. *)	     
+(** Raised by all the functions of this module when an error is encountered. *)
 exception ERROR of error
 
-(** Return a string describing the error.*)		     
+(** Return a string describing the error.*)
 val report_error : error -> string
 			      
-(** [write_big b name path ] writes an svg representation of bigraph
-    [b] on file [name] in directory [path]. Return the number of bytes written. *)
+(** [write_big b name path ] writes an svg representation of bigraph [b] on file
+    [name] in directory [path] and returns the number of bytes written. *)
 val write_big : Big.bg -> string -> string -> int
 
 (** Same as {!Export.write_big} but for transition systems. *)
@@ -31,5 +31,5 @@ val write_ts_prism : Brs.ts -> string -> string -> int
 (** Same as {!Export.write_ts_prism} but for Continuous Time Markov Chains. *)
 val write_ctmc_prism : Sbrs.ctmc -> string -> string -> int
 
-(** Export a labelling function to a text file in PRISM format. *) 
+(** Export a labelling function to a text file in PRISM format. *)
 val write_csl : (int, int) Hashtbl.t -> string -> string -> int
