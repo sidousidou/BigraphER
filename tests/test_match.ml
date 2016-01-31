sidousidou/BigraphER

commit 1f59d7710babb5e94e498cde024499a75e89cc22

Use functional interface for sparse matrices

lib/sparse.ml
@@ -1,252 +1,224 @@
-open Base
+let int_compare a b = a - b
+       
+module M_int =
+  Map.Make (struct
+	       type t = int
+	       let compare = int_compare
+	     end)
 
 type bmatrix =
   { r: int;
     c: int;
-    r_major: int H_int.t; (* Row-major order: i -> j,...,j' *)
-    c_major: int H_int.t; (* Column-major order: j -> i,...,i' *)
+    r_major: IntSet.t M_int.t; (* Row-major order: i -> j,...,j' *)
+    c_major: IntSet.t M_int.t; (* Column-major order: j -> i,...,i' *)
   }
 
 (* Create an empty matrix *)
-let make rows cols =
-  assert (rows >= 0);
-  assert (cols >= 0);
-  { r = rows;
-    c = cols;
-    r_major = H_int.create rows;
-    c_major = H_int.create cols;
+let make r c =
+  assert (r >= 0);
+  assert (c >= 0);
+  { r;
+    c;
+    r_major = M_int.empty;
+    c_major = M_int.empty;
   }
 
-let copy m =
-  { m with
-    r_major = H_int.copy m.r_major;
-    c_major = H_int.copy m.c_major;
-  }
-
-let ( = ) a b = 
-  (a.r = b.r) && (a.c = b.c) && 
-    (a.r_major = b.r_major) && (a.c_major = b.c_major)
+let equal a b =
+  a.r = b.r && a.c = b.c &&
+    M_int.equal IntSet.equal a.r_major b.r_major
 
 let compare a b =
   match a.r - b.r with
-  | 0 ->
-     (match a.c - b.c with
-      | 0 ->
-	 (match compare a.r_major b.r_major with
-	  | 0 -> compare a.c_major b.c_major
+  | 0 -> (match a.c - b.c with
+	  | 0 -> (match M_int.compare IntSet.compare a.r_major b.r_major with
+		  | 0 -> M_int.compare IntSet.compare a.c_major b.c_major
+		  | x -> x)
 	  | x -> x)
-      | x -> x)
   | x -> x
 
 let to_string m =
   let buff = Array.make_matrix m.r m.c "0" in
-  H_int.iter (fun i j -> 
-		buff.(i).(j) <- "1") m.r_major;
+  M_int.iter (fun i js ->
+	      IntSet.iter (fun j ->
+			   buff.(i).(j) <- "1")
+			  js)
+	     m.r_major;
   buff
   |> Array.map (fun r ->
 		String.concat "" (Array.to_list r))
   |> Array.to_list 
   |> String.concat "\n"
-		
-let row n =
-  assert (n >= 0);
-  let m = make 1 n in (* inline to speed up *)
-  for i = n - 1 downto 0 do (* fold returns the columns in ascending order *)
-    (* 0 -> 0,...,n-1 *)
-    H_int.add m.r_major 0 i;
-    (* 0,...,n-1 -> 0 *)
-    H_int.add m.c_major i 0
-  done;
-  m 
 
-let col n =
-  assert (n >= 0);
-  let m = make n 1 in (* inline to speed up *)
-  for i = n - 1 downto 0 do
-    (* 0,...,n-1 -> 0 *)  
-    H_int.add m.r_major i 0;
-    (* 0 -> 0,...,n-1 *)
-    H_int.add m.c_major 0 i 
-  done;
-  m 
-
-let diag n =
-  assert (n >= 0);
-  let m = make n n in
-  for i = 0 to n - 1 do
-    H_int.add m.r_major i i;
-    H_int.add m.c_major i i;
-  done;
-  m
-
-let tens a b =
-  let m = make (a.r + b.r) (a.c + b.c) in
-  (* Insert elements of a *)
-  H_int.iter (fun i j -> 
-		H_int.add m.r_major i j;
-		H_int.add m.c_major j i)
-	       a.r_major;
-  (* Insert elements of b *)
-  H_int.iter (fun i j -> 
-		H_int.add m.r_major (i + a.r) (j + a.c);
-		H_int.add m.c_major (j + a.c) (i + a.r))
-	       b.r_major;
-  m
-    
-let append (a : bmatrix) (b : bmatrix) =
-  assert (Pervasives.(=) a.r b.r);
-  let m = make a.r (a.c + b.c) in
-  (* Insert elements of a *)
-  H_int.iter (fun i j -> 
-		H_int.add m.r_major i j;
-		H_int.add m.c_major j i)
-	       a.r_major;
-  (* Insert elements of b *)
-  H_int.iter (fun i j -> 
-		H_int.add m.r_major i (j + a.c);
-		H_int.add m.c_major (j + a.c) i)
-	       b.r_major;
-  m
-    
-let stack a b =
-  assert (Pervasives.(=)  a.c b.c);
-  let m = make (a.r + b.r) a.c in
-  (* Insert elements of a *)
-  H_int.iter (fun i j -> 
-		H_int.add m.r_major i j;
-		H_int.add m.c_major j i)
-	       a.r_major;
-  (* Insert elements of b *)
-  H_int.iter (fun i j -> 
-		H_int.add m.r_major (i + a.r) j;
-		H_int.add m.c_major j (i + a.r))
-	       b.r_major;
-  m
-
-let apply_rows_exn iso m =
-  assert (Pervasives.(=) (Iso.cardinal iso) m.r);
-  let m' = make m.r m.c in
-  H_int.iter (fun i j ->
-		let i' = Iso.apply_exn iso i in
-		H_int.add m'.r_major i' j;
-		H_int.add m'.c_major j i')
-	       m.r_major;
-  m'
-
-let apply_cols_exn iso m =
-  assert (Pervasives.(=) (Iso.cardinal iso) m.c);
-  let m' = make m.r m.c in
-  H_int.iter (fun i j ->
-		let j' = Iso.apply_exn iso j in
-		H_int.add m'.r_major i j';
-		H_int.add m'.c_major j' i)
-	       m.r_major;
-  m'
-
-let apply_exn iso m =
-  assert (Pervasives.(=) (Iso.cardinal iso) m.r);
-  assert (Pervasives.(=) m.r m.c);
-  let m' = make m.r m.c in
-  H_int.iter (fun i j ->
-		let (i', j') = 
-		  (Iso.apply_exn iso i, Iso.apply_exn iso j) in
-		H_int.add m'.r_major i' j';
-		H_int.add m'.c_major j' i')
-	       m.r_major;
-  m'
-
-let add m i j =
+let add_m i j m =
+  try
+    let js = M_int.find i m in
+    M_int.add i (IntSet.add j js) m
+  with
+  | Not_found -> M_int.add i (IntSet.singleton j) m
+  		   
+let add i j m =
   assert (i >= 0);
   assert (j >= 0);
   assert (i < m.r);
   assert (j < m.c);
-  H_int.add m.r_major i j;
-  H_int.add m.c_major j i
+  { m with r_major = add_m i j m.r_major;
+	   c_major = add_m j i m.c_major;
+  }
 
+let flip_major m =
+  M_int.fold (fun i js acc ->
+	      IntSet.fold (fun j acc ->
+			   add_m i j acc)
+			  js acc)
+	     m M_int.empty
+    
+let off o_r o_c m =
+  M_int.fold (fun i js acc ->
+	      M_int.add (i + o_r) (IntSet.off o_c js) acc)
+	     m M_int.empty
+
+let merge_f _ l r =
+  match (l, r) with
+  | (Some s, Some s') -> Some (IntSet.union s s')
+  | (Some s, None) | (None, Some s) -> Some s
+  | (None, None) -> None
+    
+let row n =
+  assert (n >= 0);
+  let indexes = IntSet.of_int n in
+  IntSet.fold (fun j acc -> add 0 j acc) indexes (make 1 n)
+  
+let col n =
+  assert (n >= 0);
+  let indexes = IntSet.of_int n in
+  IntSet.fold (fun i acc -> add i 0 acc) indexes (make n 1)
+
+let diag n =
+  assert (n >= 0);
+  let indexes = IntSet.of_int n in
+  IntSet.fold (fun i acc -> add i i acc) indexes (make n n)
+  
+let tens a b =  
+  { r = a.r + b.r;
+    c = a.c + b.c;
+    r_major = M_int.merge merge_f a.r_major (off a.r a.c b.r_major);
+    c_major = M_int.merge merge_f a.c_major (off a.c a.r b.c_major);
+  }
+    
+let append (a : bmatrix) (b : bmatrix) =
+  assert (a.r = b.r);
+  { r = a.r;
+    c = a.c + b.c;
+    r_major = M_int.merge merge_f a.r_major (off 0 a.c b.r_major);
+    c_major = M_int.merge merge_f a.c_major (off a.c 0 b.c_major);
+  }
+    
+let stack a b =
+  assert (a.c = b.c);
+  { r = a.r + b.r;
+    c = a.c;
+    r_major = M_int.merge merge_f a.r_major (off a.r 0 b.r_major);
+    c_major = M_int.merge merge_f a.c_major (off 0 a.r b.c_major);
+  }
+
+let apply_rows_exn iso m =
+  assert ((Iso.cardinal iso) = m.r);
+  let (r_major, c_major) =
+    M_int.fold (fun i js (acc_r, acc_c) ->
+		let i' = Iso.apply_exn iso i in
+		(M_int.add i' js acc_r,
+		 IntSet.fold (fun j acc ->
+			      M_int.add j (IntSet.singleton i') acc) js acc_c))
+	       m.r_major (M_int.empty, M_int.empty) in
+  { m with r_major;
+	   c_major;
+  }
+
+let apply_cols_exn iso m =
+  assert ((Iso.cardinal iso) = m.c);
+  let (r_major, c_major) =
+    M_int.fold (fun j is (acc_r, acc_c) ->
+		let j' = Iso.apply_exn iso j in
+		(IntSet.fold (fun i acc ->
+			      M_int.add i (IntSet.singleton j') acc) is acc_r,
+		 M_int.add j' is acc_c))
+	       m.c_major (M_int.empty, M_int.empty) in
+  { m with r_major;
+	   c_major;
+  }
+
+let apply_exn iso m =
+  assert ((Iso.cardinal iso) = m.r);
+  assert (m.r = m.c);
+  apply_rows_exn iso m
+  |> apply_cols_exn iso
+  
 let parse_vectors adj rows =
   assert (rows >= 0);
-  let m = make rows (List.length adj) in
-  List.iteri (fun j i_list ->
-	      List.iter (fun i -> add m i j) i_list)
-	     adj;
-  m
-    
+  List.fold_left (fun (j, acc) i_list ->
+		  (j + 1,
+		   List.fold_left (fun acc i -> add i j acc) acc i_list))
+		 (0, make rows (List.length adj)) adj
+  |> snd
+
 let chl m i =
   assert (i >= 0);
   assert (i < m.r);
-  H_int.find_all m.r_major i
-
-let chl_set m i = IntSet.of_list (chl m i)		   
+  try M_int.find i m.r_major with
+  | Not_found -> IntSet.empty
 
 let prn m j =
   assert (j >= 0);
   assert (j < m.c);
-  H_int.find_all m.c_major j
-
-let prn_set m j = IntSet.of_list (prn m j)
+  try M_int.find j m.c_major with
+  | Not_found -> IntSet.empty
 		   
 let mul a b =
-  assert (Pervasives.(=) a.c b.r);
-  let m = make a.r b.c 
-  and acc = H_int.create a.r in
-  H_int.iter
-    (fun i j ->
-     let vec = H_int.find_all b.r_major j in
-     List.iter (fun k ->
-		if List.mem k (H_int.find_all acc i) then () 
-		else 
-		  (H_int.add m.r_major i k;
-		   H_int.add m.c_major k i;
-		   H_int.add acc i k;))
-	       vec) 
-    a.r_major;
-  m
+  assert (a.c = b.r);
+  let m = make a.r b.c in 
+  M_int.fold (fun i js acc ->
+	      M_int.fold (fun j is acc ->
+			  if IntSet.is_empty (IntSet.inter js is) then acc
+			  else add i j acc)
+			 b.c_major acc)
+	     a.r_major m
 
-let trans m =
-  let t = copy m in
-  let rec fix () =
-    let count =
-      H_int.fold (fun i j acc ->
-		    acc @ (List.map (fun c -> (i, c)) (chl m j))) 
-		   t.r_major []
-      |> List.fold_left (fun acc (i, c) ->
-			 if List.mem c (H_int.find_all t.r_major i)
-			 then acc
-			 else
-			   (H_int.add t.r_major i c;
-			    H_int.add t.c_major c i;
-			    acc + 1))
-			0 in
-    if count > 0 then fix ()
-    else t in
-  fix ()
+let sum a b =
+  assert (a.r = b.r);
+  assert (a.c = b.c);
+  M_int.fold (fun i js acc ->
+	      IntSet.fold (fun j acc ->	add i j acc) js acc)
+	     b.r_major a
+  
+let trans m0 =
+  let rec fix m acc =
+    let m' = mul m0 m in
+    if equal m m' then acc 
+    else fix m' (sum m' acc) in
+  fix m0 m0
 
 let dom m =
-  H_int.fold (fun i _ acc ->
-		IntSet.add i acc)
-	       m.r_major IntSet.empty
-
+  M_int.bindings m.r_major
+  |> List.split 
+  |> fst
+  |> IntSet.of_list
+       
 let codom m =
-  H_int.fold (fun j _ acc ->
-		IntSet.add j acc)
-	       m.c_major IntSet.empty
-
-let rec _iter i acc d =
-  if i < 0 then acc
-  else if IntSet.mem i d then _iter (i - 1) acc d
-  else _iter (i - 1) (IntSet.add i acc) d
-	       
+  M_int.bindings m.c_major
+  |> List.split 
+  |> fst
+  |> IntSet.of_list
+ 
 let leaves m =
-  _iter (m.r - 1) IntSet.empty (dom m)
-
+  IntSet.diff (IntSet.of_int m.r) (dom m)
+  
 let orphans m =
-  _iter (m.c - 1) IntSet.empty (codom m)
+  IntSet.diff (IntSet.of_int m.r) (codom m)
 
 let siblings m j = 
-  prn m j
-  |> List.fold_left (fun acc i ->
-		     IntSet.union acc (IntSet.of_list (chl m i))) 
-		    IntSet.empty
+  IntSet.fold (fun i acc ->
+	       IntSet.union acc (chl m i)) 
+	      (prn m j) IntSet.empty
   |> IntSet.remove j
 		
 let siblings_chk m =
@@ -255,39 +227,110 @@ let siblings_chk m =
 		 (codom m)
 		 
 let partners m i =
-  chl m i
-  |> List.fold_left (fun acc j ->
-		     IntSet.union acc (IntSet.of_list (prn m j)))
-		    IntSet.empty
+  IntSet.fold (fun j acc ->
+	       IntSet.union acc (prn m j))
+	      (chl m i) IntSet.empty
   |> IntSet.remove i 
 
 let partners_chk m =
   IntSet.for_all (fun i ->
-		  IntSet.is_empty (partners m i)) (dom m)
+		  IntSet.is_empty (partners m i))
+		 (dom m)
 
 let iter f m = 
-  H_int.iter f m.r_major
+  M_int.iter (fun i js ->
+	      IntSet.iter (fun j -> f i j) js)
+	     m.r_major
 
 let fold f m acc = 
-  H_int.fold f m.r_major acc
+  M_int.fold (fun i js acc ->
+	      IntSet.fold (fun j acc -> f i j acc) js acc)
+	     m.r_major acc
 
 let add_list m =
-  List.iter (fun (i, j) -> add m i j)
+  List.fold_left (fun acc (i, j) -> add i j acc) m
 
 let entries m =
-  H_int.length m.r_major
+  M_int.fold (fun _ js acc ->
+	      acc + (IntSet.cardinal js))
+	     m.r_major 0
 
 let levels m = 
   (* m is a graph *)
-  assert (Pervasives.(=) m.r m.c);
+  assert (m.r = m.c);
   let rec fix acc nodes res =
     (* find nodes with all children in acc *)
     let leaves = 
       IntSet.filter (fun i ->
-		     IntSet.subset (IntSet.of_list (chl m i)) acc)
+		     IntSet.subset (chl m i) acc)
 		    nodes in
     if IntSet.is_empty leaves then res
     else
       fix (IntSet.union acc leaves) (IntSet.diff nodes leaves) 
 	  (leaves :: res) in
   fix IntSet.empty (IntSet.of_int m.r) []
+
+ let aux_split r m =     
+     let (t, mi, b) = M_int.split r m in
+     match mi with
+     | Some js -> (t, M_int.add r js b)
+     | None -> (t, b)
+      
+let split_rows r m =
+  assert (r > 0);
+  assert (r <= m.r);
+  let (top, bottom) = aux_split r m.r_major in
+  ({ r;
+     c = m.c;
+     r_major = top;
+     c_major = flip_major top; 
+   },
+   { r = m.r - r;
+     c = m.c;
+     r_major = bottom;
+     c_major = flip_major bottom;
+   })
+
+let split_columns c m =
+  assert (c > 0);
+  assert (c <= m.c);
+  let (left, right) = aux_split c m.c_major in
+  ({ r = m.r;
+     c;
+     r_major = flip_major left;
+     c_major = left; 
+   },
+   { r = m.r;
+     c = m.c - c;
+     r_major = flip_major right;
+     c_major = right;
+   })
+
+let split r c m =
+  let (t, b) = split_rows r m in
+  (split_columns c t, split_columns c b)
+
+exception PARSE_ERROR
+    
+let parse_string r n s rows =
+  assert (r >= 0);
+  assert (n >= 0);
+  assert (s >= 0);
+  assert (List.length rows = r + n);
+  assert (List.for_all (fun l -> String.length l = n + s) rows);
+  let to_int s =
+    let rec aux i acc =
+      if i < 0 then acc
+      else match String.get s i with
+	   | '1' -> aux (i - 1) (IntSet.add i acc)
+	   | '0' -> aux (i - 1) acc
+	   | _ -> raise PARSE_ERROR in
+    aux ((String.length s) - 1) IntSet.empty in
+  let (_, r_major) = List.fold_left (fun (i, acc) r ->
+				     (i + 1, M_int.add i (to_int r) acc))
+				    (0, M_int.empty) rows in
+  { r = r + n;
+    c = n + s;
+    r_major;
+    c_major = flip_major r_major;}
+  |> split r n

lib/big.ml
@@ -855,7 +855,7 @@ let key b =
 let equal_opt a b =
   (Place.deg_roots a.p = Place.deg_roots b.p)
   && (Place.deg_sites a.p = Place.deg_sites b.p)
-  && (Sparse.(=) a.p.Place.rs b.p.Place.rs)
+  && (Sparse.equal a.p.Place.rs b.p.Place.rs)
   && (* Placing or wiring *)
     if b.n.Nodes.size = 0 then
       (Place.equal_placing a.p b.p) && (Link.Lg.equal a.l b.l)
@@ -870,7 +870,7 @@ let equal a b =
   && (Place.deg_roots a.p = Place.deg_roots b.p)
   && (Place.deg_sites a.p = Place.deg_sites b.p)
   && (Nodes.equal a.n b.n)
-  && (Sparse.(=) a.p.Place.rs b.p.Place.rs)
+  && (Sparse.equal a.p.Place.rs b.p.Place.rs)
   && (* Placing or wiring *)
     if b.n.Nodes.size = 0 then
       (Place.equal_placing a.p b.p) && (Link.Lg.equal a.l b.l)

lib/intSet.ml
@@ -15,7 +15,7 @@ let of_list =
 		  add e acc)
 		 empty
 		 
-(* given a non-nagative integer i return ordinal i = {0,1,....,i-1} i.e.   *)
+(* given a non-negative integer i return ordinal i = {0,1,....,i-1} i.e.   *)
 (* set with cardinality i                                                  *)
 let of_int i =
   assert (i >= 0);

commit a021e72a702daf19113d8ee26f3d44274560601e

Work on bugs introduced in 1f59d77

lib/sparse.ml
@@ -37,9 +37,11 @@ let compare a b =
   | x -> x
 
 let to_string m =
+  (* Printf.printf "Sparse.to_string: %d %d\n" m.r m.c; *)
   let buff = Array.make_matrix m.r m.c "0" in
   M_int.iter (fun i js ->
 	      IntSet.iter (fun j ->
+			   (* Printf.printf "printing (%d, %d)\n" i j; *)
 			   buff.(i).(j) <- "1")
 			  js)
 	     m.r_major;
@@ -275,11 +277,13 @@ let levels m =
      match mi with
      | Some js -> (t, M_int.add r js b)
      | None -> (t, b)
-      
+
+(* Dual of stack *)		 
 let split_rows r m =
   assert (r > 0);
   assert (r <= m.r);
-  let (top, bottom) = aux_split r m.r_major in
+  let (top, b) = aux_split r m.r_major in
+  let bottom = off (-r) 0 b in
   ({ r;
      c = m.c;
      r_major = top;
@@ -291,10 +295,12 @@ let split_rows r m =
      c_major = flip_major bottom;
    })
 
+(* Dual of append *)    
 let split_columns c m =
   assert (c > 0);
   assert (c <= m.c);
-  let (left, right) = aux_split c m.c_major in
+  let (left, right') = aux_split c m.c_major in
+  let right = off (-c) 0 right' in
   ({ r = m.r;
      c;
      r_major = flip_major left;

tests/junit.ml
@@ -7,7 +7,7 @@ let string_of_attributes tag attr =
 		     
 let xml_block tag attr chl_blocks =
   sprintf "<%s>\n\
-             %s\n\
+             %s\
            </%s>"
 	  (string_of_attributes tag attr)
 	  (String.concat "\n" (List.filter (fun s -> String.length s > 0) chl_blocks))

tests/test_decomp.ml
@@ -50,15 +50,16 @@ let do_tests =
 		__MODULE__,
 		xml_block "system-out" [] ["Test passed. Place graph not decomposable into prime components."],
 		[])
-            | e ->
+            | _ ->
 	       (n,
 		__MODULE__,
 		xml_block "system-out" [] [error_msg],
-		[xml_block "error" attr_err [Printexc.to_string e]]))
+		[xml_block "error" attr_err [Printexc.get_backtrace ()]]))
 	       
  
 (* Args: PATH OUT-PATH FNAME *)  
 let () =
+  Printexc.record_backtrace true;
   let bg_strings = parse_all Sys.argv.(1) in
   let bgs = 
     List.filter (fun (_, b) -> 

tests/test_match.ml
@@ -85,7 +85,12 @@ let do_tests =
 						  (comp c (comp (tens t.pattern id) d))))
 					     occs
 				   |> String.concat "\n")],
-     [xml_block "failure" attr_match [msg]]) in
+     [xml_block "failure" attr_match [msg]])
+  and error n0 b0 n1 b1 =
+    (* print_endline (n0 ^ " &gt; " ^ n1); *)
+    n0 ^ "\n" ^ (Big.to_string b0) ^ "\n"
+    ^  n1 ^ "\n" ^ (Big.to_string b1) ^ "\n"
+    ^ (Printexc.get_backtrace ()) in
   List.map (fun t ->
 	    let default_fail_msg = sprintf "%s cannot be matched in %s." t.p_name t.t_name in
 	    try
@@ -106,12 +111,12 @@ let do_tests =
 		| ("T14", "P25") -> success t
 		| _ -> failure t (sprintf "Interfaces %s != %s"
 					  (string_of_inter x) (string_of_inter y)) [])
-            |  e ->
+            |  _ ->
 		(t.t_name ^ " &gt; " ^ t.p_name,
 		 module_name,
 		 xml_block "system-out" [] [error_msg],
-		 [xml_block "error" attr_err [Printexc.to_string e]]))
-
+		 [xml_block "error" attr_err
+			    [error t.t_name t.target t.p_name t.pattern]]))
 
 let do_equality_tests l ts =
   let success s msg =
@@ -123,18 +128,20 @@ let do_equality_tests l ts =
     (s,
      module_name,
      xml_block "system-out" [] [msg_out],
-     [xml_block "failure" attr_match [msg]]) in
+     [xml_block "failure" attr_match [msg]])
+  and error n b =
+    n ^ "\n" ^ (Big.to_string b) ^ "\n" ^ (Printexc.get_backtrace ()) in
   (List.map (fun (n, b) ->
 	     let s = n ^ " = " ^ n in
              try
                if Big.equal b b then success s "Bigraphs are equal"
                else failure s "Bigraphs are not equal" (sprintf "%s != %s" n n)
              with
-             | e ->
+             | _ ->
 		(s,
 		 module_name,
 		 xml_block "system-out" [] [error_msg],
-		 [xml_block "error" attr_err [Printexc.to_string e]])
+		 [xml_block "error" attr_err [error n b]])
 	    ) (List.sort (fun (x, _) (y, _) -> String.compare x y) l))
   @ (List.map (fun t ->
 	       let s = t.t_name ^ " = " ^ t.p_name in
@@ -145,11 +152,14 @@ let do_equality_tests l ts =
 		    |  _ -> failure s "Bigraphs are equal" s)
 		 else success s "Bigraphs are not equal"
 	       with
-               | e -> 
+               | _ -> 
 		  (s,
 		   module_name,
 		   xml_block "system-out" [] [error_msg],
-		   [xml_block "error" attr_err [Printexc.to_string e]])
+		   [xml_block "error" attr_err
+			      [ t.t_name ^ "\n" ^ (Big.to_string t.target) ^ "\n"
+				 ^  t.p_name ^ "\n" ^ (Big.to_string t.pattern) ^ "\n"
+				 ^ (Printexc.get_backtrace ()) ]])
 	      ) ts)
 
 let safe_exp f =
@@ -508,6 +518,7 @@ let tests bgs = (* TEST 1 *)
 
 (* Args: PATH PATH-out*)  
 let () =
+  Printexc.record_backtrace true;
   let bg_strings = parse_all Sys.argv.(1) in
   let bgs =
     List.map (fun (n, ls) -> (n, (Big.parse ls))) bg_strings in