406ef690b152097792e23b1ddc517c7976f4a627
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2016-02-09 20:13:07 +0000

    Add support for code coverage

---
+++ scripts/shippable-cover.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+eval `opam config env`
+
+PREFIX=$(opam config var prefix)
+XML_DIR=shippable/codecoverage
+BISECT_DIR=shippable/bisect
+OBJ=_build
+
+echo "Cleaning"
+
+ocaml setup.ml -uninstall
+ocamlfind remove bigraph
+ocaml setup.ml -clean
+
+echo "Compiling"
+
+sed -i 's/BuildDepends:/BuildDepends: bisect_ppx,/g' _oasis
+oasis setup
+
+ocaml setup.ml -configure --enable-tests --prefix $PREFIX
+
+echo "Running tests"
+
+ocaml setup.ml -test
+
+echo "Generating coverage reports"
+
+bisect-report bisect*.out -I $OBJ -text $BISECT_DIR/report
+bisect-report bisect*.out -I $OBJ -summary-only -text $BISECT_DIR/summary
+(cd $OBJ;
+ bisect-report ../bisect*.out -html ../$BIDECT_DIR/html;
+ bisect-report ../bisect*.out -xml ../$XML_DIR/report.xml;
+ bisect-report ../bisect*.out -xml-emma ../$XML_DIR/report-emma.xml)
+
+cat $BISECT_DIR/summary
--- scripts/shippable-link.sh
+++ scripts/shippable-link.sh
@@ -11,11 +11,15 @@ ocaml setup.ml -distclean
 
 cp ./examples/ex.ml $ROOT/ex.ml
 
+echo "Linking bytecode"
+
 ocamlfind ocamlc -o $ROOT/ex.byte -package bigraph -linkpkg $ROOT/ex.ml
 if $ROOT/ex.byte | cmp -s -  $SRC/ex.reference &> /dev/null
   then cp $SRC/byte-0.xml $TEST/byte-link.xml
   else cp $SRC/byte-1.xml $TEST/byte-link.xml
 fi
+
+echo "Linking native code"
 
 ocamlfind ocamlopt -o $ROOT/ex.native -package bigraph -linkpkg $ROOT/ex.ml
 if $ROOT/ex.native | cmp -s -  $SRC/ex.reference &> /dev/null
