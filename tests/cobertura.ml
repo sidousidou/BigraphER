399820c056f6e54791a56da3780ddb14facb4b03
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2016-02-29 02:18:16 +0000

    Add branch-rate and fix numeric format

--- tests/cobertura.ml
+++ tests/cobertura.ml
@@ -1,6 +1,10 @@
 (* parser for bisect files *)
 
-type item = String.t * float
+type item =
+  { kind : String.t;
+    cover : float;
+    n : float;
+    total : float; }
 			 
 type mod_cover =
   { name : String.t;
@@ -11,19 +15,32 @@ let is_item l =
 
 let first_token l c_start c_end =
   let i = String.index l c_start in
-  let j = String.index_from l (i + 1)
-			    c_end in
+  let j = String.index_from l (i + 1) c_end in
   String.sub l (i + 1) (j - i - 1)
 			 
 let parse_item l =
-  let name =
+  let kind =
     try first_token l '\'' '\'' with
     | Not_found -> "total"
-  and percent =
+  and cover =
     try
       (float_of_string (first_token l '(' '%')) /. 100. with
-    | Not_found -> 1.0 in
-  (name, percent)
+    | Not_found -> 1.0
+  and (n, total) =
+    try
+      let colon_i = String.index l ':' in
+      let i = String.index_from l (colon_i + 1) '/' in
+      (let j =
+	 String.rindex_from l (i - 1) ' ' in
+       String.sub l (j + 1) (i - j - 1)
+       |> float_of_string,
+       let j' =
+	 String.index_from l (i + 1) ' ' in
+       String.sub l (i + 1) (j' - i -1)
+       |> float_of_string)
+    with
+    | Not_found -> (0., 0.) in
+  {kind; cover; n; total;}
     
 let parse_bisect_report = function
   | [] -> assert false
@@ -40,11 +57,24 @@ let parse_bisect_report = function
   |> fun (a, _, _) -> a
   |> List.rev
 
-let get_rate m =
-  m.items |> List.rev |> List.hd |> snd |> string_of_float
-       
+let get_line_rate m =
+  (m.items |> List.rev |> List.hd).cover
+  |> string_of_float
+
+let get_branch_rate m =
+  m.items
+  |> List.filter (fun i -> i.kind = "if/then"
+			   || i.kind = "try"
+			   || i.kind = "match/function")
+  |> List.fold_left (fun (n_acc, tot_acc) i ->
+		     (n_acc +. i.n, tot_acc +. i.total))
+		    (0.0, 0.0)
+  |> fun (a, b) -> a /. b
+  |> string_of_float
+					     
 let to_attribs m v =
-  "line-rate=\"" ^ (get_rate m) ^ "\" "
+  "line-rate=\"" ^ (get_line_rate m) ^ "\" "
+  ^ "branch-rate=\"" ^ (get_branch_rate m) ^ "\" "
   ^ "version=\"" ^ v ^ "\" "  
   ^ "timestamp=\"" ^ (string_of_float (Unix.time ())) ^ "\""
 
@@ -62,7 +92,8 @@ let to_packages ms =
   List.map (fun m ->
 	    "<package "
 	    ^ "name=\"" ^ m.name ^ "\" "
-	    ^ "line-rate=\"" ^ (get_rate m) ^ "\""
+	    ^ "line-rate=\"" ^ (get_line_rate m) ^ "\" "
+	    ^ "branch-rate=\"" ^ (get_branch_rate m) ^ "\" "
 	    ^ ">\n<classes></classes>\n</package>") ms
   |> String.concat "\n" 
 
