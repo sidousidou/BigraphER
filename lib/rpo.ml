4d66a899c7b184983aa39b0f561bab45ce685348
Michele Sevegnani <Michele.Sevegnani@glasgow.ac.uk>
2016-08-04 16:51:13 +0100

    Add tests and input isos

--- lib/rpo.ml
+++ lib/rpo.ml
@@ -1,19 +1,49 @@
+type bound = Big.bg * Big.bg
 
-let makeSet iso =
-  IntSet.of_list (Iso.codom iso)
- 
+type rpo = Big.bg * Big.bg * Big.bg
+
+(* Sets of node identifiers {0, ..., size - 1} for a bound *)                 
+let node_sets b =
+  let aux b f = IntSet.of_int ((f b).Big.n.Nodes.size)
+  in (aux b fst, aux b snd)
+                 
 (* RPO algorithm *)
-(* The isomorphism shows for each bigraph which node (domain) is connected to which concrete node (codomain)
-   in the rpo model *)
-let rpo a0 iso_a0 a1 iso_a1 d0 iso_d0 d1 iso_d1 =
-  let va0 = makeSet iso_a0 in
-  let va1 = makeSet iso_a1 in
-  let vd0 = makeSet iso_d0 in
-  let vd1 = makeSet iso_d1 in
+(* The isomorphism are as follows:
+   - i_a0_a1: a0 -> a1 
+   - i_a0_d1: a0 -> d1 
+   - i_d0_a1: d0 -> a1 
+   - i_d0_d1: d0 -> d1
+ *)
+let rpo a d i_a0_a1 i_a0_d1 i_d0_a1 i_d0_d1 =
+  let (va0, va1) = node_sets a in
+  let (vd0, vd1) = node_sets d in
   (* Now to the actual algorithm *)
-  let vb0 = IntSet.diff va1 va0 in
-  let vb1 = IntSet.diff va0 va1 in
-  let vb = IntSet.inter vd0 vd1 in
+  let vb0 = IntSet.diff va1 (Iso.codom i_a0_a1 |> IntSet.of_list) in  (* identifiers in a1 *)
+  let vb1 = IntSet.diff va0 (Iso.dom i_a0_a1 |> IntSet.of_list) in (* identifiers in a0 *)
+  let vb = IntSet.inter vd0 (Iso.dom i_d0_d1 |> IntSet.of_list) in (* identifiers in d0 *)
   (* Only dummy code for compilation *)
-  3 + 4
-  
+  (Big.id_eps, Big.id_eps, Big.id_eps)
+
+(* Run tests here *)
+let () =
+  print_endline "Tests for RPO algorithm";
+  let a0 = Big.id_eps 
+  and a1 = Big.id_eps
+  and d0 = Big.id_eps
+  and d1 = Big.id_eps
+  and b0 = Big.id_eps
+  and b1 = Big.id_eps
+  and b =  Big.id_eps
+  and i_a = Iso.of_list_exn []
+  and i_a' = Iso.of_list_exn []
+  and i_d = Iso.of_list_exn []
+  and i_d' = Iso.of_list_exn [] in
+  assert (Big.equal (Big.comp d0 a0) (Big.comp d1 a1));
+  assert (Big.equal (Big.comp b b0) d0);
+  assert (Big.equal (Big.comp b b1) d1);
+  let (b0', b1', b') = rpo (a0, a1) (d0, d1) i_a i_a' i_d i_d' in
+  assert (Big.equal b0 b0');
+  assert (Big.equal b1 b1');
+  assert (Big.equal b b');
+  print_endline "Test OK"
+                
--- lib/rpo.mli
+++ lib/rpo.mli
@@ -1,2 +1,5 @@
+type bound = Big.bg * Big.bg
+type rpo = Big.bg * Big.bg * Big.bg
+                        
 (** Four bigraphs Ai and Di as well as isomporphisms for each to denote the node to node relations **)
-val rpo: Big.bg -> int Iso.t -> Big.bg -> int Iso.t -> Big.bg -> int Iso.t -> Big.bg -> int Iso.t -> int
+val rpo: bound -> bound -> int Iso.t -> int Iso.t -> int Iso.t -> int Iso.t -> rpo
