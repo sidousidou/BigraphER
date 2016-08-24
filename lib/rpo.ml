6a173803b137f29f54ef49453db38292e8a4cd71
David Frohlingsdorf <2079884f@student.gla.ac.uk>
2016-08-24 15:49:01 +0000

    Fixed shared parent issue

--- lib/rpo.ml
+++ lib/rpo.ml
@@ -13,15 +13,17 @@ let node_sets b =
   in (aux b fst, aux b snd)
 
 (* Give the root parent set of a node (either empty set or singleton) *)
-let rootPrntN n b =
+let rootPrntN n b red =
   let prntSet = Sparse.prn b.Big.p.Place.rn n in
   if IntSet.equal prntSet IntSet.empty then None
+  else if IntSet.subset prntSet red then None 
   else Some (IntSet.choose prntSet)
 
 (* As before but for sites rather than nodes *)
-let rootPrntS s b =
+let rootPrntS s b red =
   let prntSet = Sparse.prn b.Big.p.Place.rs s in
   if IntSet.equal prntSet IntSet.empty then None
+  else if IntSet.subset prntSet red then None
   else Some (IntSet.choose prntSet)
 
 let rec rootEqui mHat tuple =
@@ -30,6 +32,35 @@ let rec rootEqui mHat tuple =
                 else (rootEqui tl tuple) 
   | [] -> TupleSet.empty
 
+ 
+let rec _buildRed n s a0 a1 red0 red1 iso =
+  if IntSet.is_empty n then (* Nodes or sides? *)
+    (if IntSet.is_empty s then (red0, red1) (* Base case *)
+    else
+      let w = IntSet.choose s in
+      match (rootPrntS w a0 red0) with
+      | Some r0 ->
+        (match (rootPrntS w a1 red1) with
+        | Some _ -> _buildRed n (IntSet.remove w s) a0 a1 red0 red1 iso
+        | None -> buildRed a0 a1 (IntSet.add r0 red0) red1 iso)
+      | None ->
+        (match (rootPrntS w a1 red1) with
+        | Some r1 -> buildRed a0 a1 red0 (IntSet.add r1 red1) iso
+        | None -> _buildRed n (IntSet.remove w s) a0 a1 red0 red1 iso))
+  else
+    let w = IntSet.choose n in
+    match (rootPrntN w a0 red0) with
+    | Some r0 ->
+      (match (rootPrntN (Iso.apply_exn iso w) a1 red1) with
+      | Some _ -> _buildRed (IntSet.remove w n) s a0 a1 red0 red1 iso
+      | None -> buildRed a0 a1 (IntSet.add r0 red0) red1 iso)
+    | None ->
+      (match (rootPrntN (Iso.apply_exn iso w) a1 red1) with
+      | Some r1 -> buildRed a0 a1 red0 (IntSet.add r1 red1) iso
+      | None -> _buildRed (IntSet.remove w n) s a0 a1 red0 red1 iso)
+and buildRed a0 a1 red0 red1 iso = 
+  _buildRed (Iso.dom iso |> IntSet.of_list) (a0.Big.p.Place.s |> IntSet.of_int) a0 a1 red0 red1 iso  
+ 
 (* From Real World OCaml book, page 51. Drops value of list *)
 let rec drop_value l to_drop =
   match l with
@@ -38,15 +69,11 @@ let rec drop_value l to_drop =
     let new_tl = drop_value tl to_drop in
     if hd = to_drop then new_tl else hd :: new_tl
 
-let joinSets (r0, r1) acc = (* Lines 18 to 28 of pseudo code *)
+let joinSets (r0, r1) acc = (* Lines 18 to 22 of pseudo code *)
   let s = rootEqui acc (0,r0) in
   let t = rootEqui acc (1,r1) in
   let redAcc = drop_value (drop_value acc t) s in
-  let newS = if (TupleSet.equal s TupleSet.empty) then TupleSet.singleton (0,r0) 
-             else s in
-  let newT = if (TupleSet.equal t TupleSet.empty) then TupleSet.singleton (1,r1) 
-             else t in
-  (TupleSet.union newS newT) :: redAcc
+  (TupleSet.union s t) :: redAcc
 
 (* Returns the integer position in which the set resides *)
 let rec listPos ?(acc=0) l set =
@@ -79,37 +106,26 @@ let rpo a d (i_a0_a1, i_a0_d1, i_d0_a1, i_d0_d1) =
   let vb1 = IntSet.diff va0 (Iso.dom i_a0_a1 |> IntSet.of_list) in (* identifiers in a0 *)
   let vb = IntSet.inter vd0 (Iso.dom i_d0_d1 |> IntSet.of_list) in (* identifiers in d0 *)
   (* Using a list of sets is easier than a set of sets. Start with the individual nodes *)
-  let rInd0 = 
-       IntSet.fold 
-          (fun i acc -> 
-             match (rootPrntN i (fst a)) with
-             | None -> acc
-             | Some r -> IntSet.add r acc) 
-          vb1 
-          IntSet.empty in
-  let rInd1 = 
-       IntSet.fold 
-          (fun i acc -> 
-             match (rootPrntN i (snd a)) with
-             | None -> acc
-             | Some r -> IntSet.add r acc)
-          vb0 
-          IntSet.empty in
-  let mHatInd = 
-       IntSet.fold 
-          (fun i acc -> TupleSet.singleton (1,i) :: acc ) 
-          rInd1
-          (IntSet.fold 
-              (fun i acc -> TupleSet.singleton (0,i) :: acc ) 
-              rInd0 
-              []) in
+  let (red0, red1) = buildRed (fst a) (snd a) IntSet.empty IntSet.empty i_a0_a1 in
+  let mHatInd =
+       IntSet.fold
+          (fun i acc ->
+             if IntSet.mem i red0 then acc
+             else TupleSet.singleton (0,i) :: acc)
+          ((fst a).Big.p.Place.r |> IntSet.of_int)
+          (IntSet.fold
+             (fun i acc ->
+                if IntSet.mem i red1 then acc
+                else TupleSet.singleton (1,i) :: acc)
+             ((snd a).Big.p.Place.r |> IntSet.of_int)
+             []) in
   (* Shared nodes -with ids from a1- *)
   let vShared = IntSet.inter va0 (Iso.dom i_a0_a1 |> IntSet.of_list) in
   let mHatShared = 
        IntSet.fold 
           (fun i acc ->
-             match ((rootPrntN i (fst a), 
-                     rootPrntN (Iso.apply_exn i_a0_a1 i) (snd a))) with
+             match ((rootPrntN i (fst a) red0, 
+                     rootPrntN (Iso.apply_exn i_a0_a1 i) (snd a) red1)) with
              | (Some r0, Some r1) -> joinSets (r0, r1) acc 
              | _ -> acc)
           vShared 
@@ -117,7 +133,7 @@ let rpo a d (i_a0_a1, i_a0_d1, i_d0_a1, i_d0_d1) =
   let mHat = 
        IntSet.fold 
           (fun i acc ->
-             match ((rootPrntS i (fst a), rootPrntS i (snd a))) with
+             match ((rootPrntS i (fst a) red0, rootPrntS i (snd a) red1)) with
              | (Some r0, Some r1) -> joinSets (r0, r1) acc 
              | _ -> acc)
           h 
@@ -184,7 +200,7 @@ let rpo a d (i_a0_a1, i_a0_d1, i_d0_a1, i_d0_d1) =
   let b0RN = 
        IntSet.fold 
           (fun i acc -> 
-             match (rootPrntN i (snd a)) with
+             match (rootPrntN i (snd a) red1) with
              | Some r -> Sparse.add (listPos mHat (rootEqui mHat (1,r))) 
                                     (Iso.apply_exn i_a1_b0 i) acc
              | None -> acc)
@@ -193,7 +209,7 @@ let rpo a d (i_a0_a1, i_a0_d1, i_d0_a1, i_d0_d1) =
   let b1RN = 
        IntSet.fold 
           (fun i acc -> 
-             match (rootPrntN i (fst a)) with
+             match (rootPrntN i (fst a) red0) with
              | Some r -> Sparse.add (listPos mHat (rootEqui mHat (0,r))) 
                                     (Iso.apply_exn i_a0_b1 i) acc
              | None -> acc)
@@ -235,7 +251,7 @@ let rpo a d (i_a0_a1, i_a0_d1, i_d0_a1, i_d0_d1) =
           (fun t acc -> 
              let (i,r) = TupleSet.choose t in
              let b = (if i==0 then (fst d) else (snd d)) in
-             match rootPrntS r b with
+             match rootPrntS r b IntSet.empty with
              | Some par -> Sparse.add par (listPos mHat t) acc 
              | None -> acc)
           mHat 
@@ -259,7 +275,7 @@ let rpo a d (i_a0_a1, i_a0_d1, i_d0_a1, i_d0_d1) =
   let bRN = 
        IntSet.fold 
           (fun n acc -> 
-             match rootPrntN n (fst d) with
+             match rootPrntN n (fst d) IntSet.empty with
              | Some par -> Sparse.add par (Iso.apply_exn i_d0_b n) acc
              | None -> acc)
           vb 
