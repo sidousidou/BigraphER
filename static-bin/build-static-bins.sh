967cb507b1cf5280f0806df38c97a1abb82e32c9
Michele Sevegnani <michele.sevegnani@glasgow.ac.uk>
2020-04-10 21:24:42 +0100

    Clean up repo

---
+++ static-bin/build-static-bins.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env sh
+
+IMG_NAME=bigrapher:static
+CON_NAME=dummy
+VERSION=1.9.1
+
+cd ..
+docker build --tag $IMG_NAME -f static-bin/Dockerfile .
+docker create --name $CON_NAME $IMG_NAME
+# libc static binaries
+cd static-bin
+docker cp $CON_NAME:/bin/bigrapher ./bigrapher
+docker cp $CON_NAME:/bin/big_match ./big_match
+tar -czf bigrapher-$VERSION+static.tar.gz bigrapher big_match
+# musl static binaries
+docker cp $CON_NAME:/bin/musl/bigrapher ./bigrapher
+docker cp $CON_NAME:/bin/musl/big_match ./big_match
+tar -czf bigrapher-$VERSION+static+musl.tar.gz bigrapher big_match
+rm -f ./bigrapher ./big_match
+docker rm -f $CON_NAME
