e1c9b265a4a77f01ef10f2c86bd3ea1f6ab36fc3
Michele Sevegnani <michele.sevegnani@glasgow.ac.uk>
2020-04-23 18:42:22 +0100

    Try to build binaries after ci when a new tag v.* is pushed

--- static-bin/build-static-bins.sh
+++ static-bin/build-static-bins.sh
@@ -3,19 +3,14 @@ set -e
 
 IMG_NAME=bigrapher:static
 CON_NAME=dummy
-VERSION=1.9.3
+VERSION=$(echo $1 | cut -c 3-)
+OUT=shippable/buildoutput
 
-cd ..
 docker build --tag $IMG_NAME -f static-bin/Dockerfile.static .
 docker create --name $CON_NAME $IMG_NAME
-# libc static binaries
-cd static-bin
-docker cp $CON_NAME:/bin/bigrapher ./bigrapher
-docker cp $CON_NAME:/bin/big_match ./big_match
-tar -czf bigrapher-$VERSION+static.tar.gz bigrapher big_match
-# musl static binaries
+cd $OUT
 docker cp $CON_NAME:/bin/musl/bigrapher ./bigrapher
 docker cp $CON_NAME:/bin/musl/big_match ./big_match
-tar -czf bigrapher-$VERSION+static+musl.tar.gz bigrapher big_match
-rm -f ./bigrapher ./big_match
+tar -czfv bigrapher-$VERSION+static+musl.tar.gz bigrapher big_match
+rm -f bigrapher big_match
 docker rm -f $CON_NAME
