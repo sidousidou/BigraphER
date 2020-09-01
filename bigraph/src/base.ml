ef2a48d05c296a1314edef68cbce8f9da37969da
Michele Sevegnani <michele.sevegnani@glasgow.ac.uk>
2020-09-01 14:53:42 +0100

    Replace some more non tailrec functions

--- bigraph/src/base.ml
+++ bigraph/src/base.ml
@@ -237,3 +237,5 @@ let cartesian a b =
   List.fold_left
     (fun acc j -> List.fold_left (fun acc j' -> (j, j') :: acc) acc b)
     [] a
+
+let list_rev_split_left l = List.fold_left (fun res (a, _) -> a :: res) [] l
